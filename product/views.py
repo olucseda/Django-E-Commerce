f44f0eb2c5a44b8c533f4822a9ab71f96d596ea9
seda.oluc@hotmail.com <68497345+olucseda@users.noreply.github.com>
2020-08-12 19:28:50 +0300

    commit

--- product/views.py
+++ product/views.py
@@ -1,6 +1,10 @@
+
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Product Page")
+    text =  "Django kurulumu"
+    context = {'text' : text}
+    return render(request, 'index.html', context)
